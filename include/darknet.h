99bfebb7d62de138fcd1c8ae9b8200c6581a2eac
max <msinev@gmail.com>
2020-06-28 18:06:22 +0300

    Shade of time

--- include/darknet.h
+++ include/darknet.h
@@ -1,6 +1,8 @@
 #ifndef DARKNET_API
 #define DARKNET_API
 
+#define MDEBUG 1
+
 #if defined(_MSC_VER) && _MSC_VER < 1900
 #define inline __inline
 #endif
@@ -104,7 +106,7 @@ typedef struct tree {
 
 // activations.h
 typedef enum {
-    LOGISTIC, RELU, RELU6, RELIE, LINEAR, RAMP, TANH, PLSE, REVLEAKY, LEAKY, ELU, LOGGY, STAIR, HARDTAN, LHTAN, SELU, GELU, SWISH, MISH, NORM_CHAN, NORM_CHAN_SOFTMAX, NORM_CHAN_SOFTMAX_MAXVAL
+    LOGISTIC, RELU, RELU6, RELIE, LINEAR, RAMP, TANH, PLSE, LEAKY, ELU, LOGGY, STAIR, HARDTAN, LHTAN, SELU, GELU, SWISH, MISH, NORM_CHAN, NORM_CHAN_SOFTMAX, NORM_CHAN_SOFTMAX_MAXVAL
 }ACTIVATION;
 
 // parser.h
@@ -180,8 +182,7 @@ typedef enum {
     LOGXENT,
     L2NORM,
     EMPTY,
-    BLANK,
-    CONTRASTIVE
+    BLANK
 } LAYER_TYPE;
 
 // layer.h
@@ -206,7 +207,6 @@ typedef struct update_args {
 struct layer {
     LAYER_TYPE type;
     ACTIVATION activation;
-    ACTIVATION lstm_activation;
     COST_TYPE cost_type;
     void(*forward)   (struct layer, struct network_state);
     void(*backward)  (struct layer, struct network_state);
@@ -225,7 +225,6 @@ struct layer {
     int flipped;
     int inputs;
     int outputs;
-    float mean_alpha;
     int nweights;
     int nbiases;
     int extra;
@@ -245,7 +244,7 @@ struct layer {
     int antialiasing;
     int maxpool_depth;
     int out_channels;
-    float reverse;
+    int reverse;
     int flatten;
     int spatial;
     int pad;
@@ -260,22 +259,18 @@ struct layer {
     int keep_delta_gpu;
     int optimized_memory;
     int steps;
-    int bottleneck;
-    float time_normalizer;
     int state_constrain;
     int hidden;
     int truth;
     float smooth;
     float dot;
     int deform;
-    int grad_centr;
     int sway;
     int rotate;
     int stretch;
     int stretch_sway;
     float angle;
     float jitter;
-    float resize;
     float saturation;
     float exposure;
     float shift;
@@ -358,15 +353,12 @@ struct layer {
     float **layers_output;
     float **layers_delta;
     WEIGHTS_TYPE_T weights_type;
-    WEIGHTS_NORMALIZATION_T weights_normalization;
+    WEIGHTS_NORMALIZATION_T weights_normalizion;
     int   * map;
     int   * counts;
     float ** sums;
     float * rand;
     float * cost;
-    int *labels;
-    float *cos_sim;
-    float *p_constrastive;
     float * state;
     float * prev_state;
     float * forgot_state;
@@ -390,7 +382,6 @@ struct layer {
     float *weight_updates;
 
     float scale_x_y;
-    int objectness_smooth;
     float max_delta;
     float uc_normalizer;
     float iou_normalizer;
@@ -525,8 +516,6 @@ struct layer {
     float *r_gpu;
     float *h_gpu;
     float *stored_h_gpu;
-    float *bottelneck_hi_gpu;
-    float *bottelneck_delta_gpu;
 
     float *temp_gpu;
     float *temp2_gpu;
@@ -607,11 +596,9 @@ struct layer {
 
     float * input_antialiasing_gpu;
     float * output_gpu;
-    float * output_avg_gpu;
     float * activation_input_gpu;
     float * loss_gpu;
     float * delta_gpu;
-    float * cos_sim_gpu;
     float * rand_gpu;
     float * drop_blocks_scale;
     float * drop_blocks_scale_gpu;
@@ -674,8 +661,6 @@ typedef struct network {
     float *output;
     learning_rate_policy policy;
     int benchmark_layers;
-    int *total_bbox;
-    int *rewritten_bbox;
 
     float learning_rate;
     float learning_rate_min;
@@ -723,11 +708,7 @@ typedef struct network {
     int attention;
     int adversarial;    
     float adversarial_lr;
-    float max_chart_loss;
     int letter_box;
-    int mosaic_bound;
-    int contrastive;
-    int unsupervised;
     float angle;
     float aspect;
     float exposure;
@@ -904,12 +885,9 @@ typedef struct load_args {
     int track;
     int augment_speed;
     int letter_box;
-    int mosaic_bound;
     int show_imgs;
     int dontuse_opencv;
-    int contrastive;
     float jitter;
-    float resize;
     int flip;
     int gaussian_noise;
     int blur;
