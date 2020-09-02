db197cff07690ce41769bddae1d9324e894f46ae
max <msinev@gmail.com>
2020-09-02 05:16:16 +0300

    Shade concurrent

--- maxsrc/shadeoftime.cpp
+++ maxsrc/shadeoftime.cpp
@@ -1,12 +1,83 @@
 //
 // Created by max on 10.06.2020.
 //
+#undef DEBUG
 #include "shadeoftime.h"
 #include "opencv2/opencv.hpp"
 #include <iostream>
+#include <thread>
+#include "tchannel.hpp"
 
 using namespace std;
 using namespace cv;
+
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+
+typedef tchannel::channel_limited<Mat,5> chanStage;
+
+void DoWrite(chanStage *in, char *path, int frameWidth, int frameHeight) {
+    Mat image;
+    cv::VideoWriter makeVideo;
+    makeVideo.open(path,
+                   VideoWriter::fourcc('X','2','6','4'), 30, cv::Size(frameWidth, frameHeight), true);
+
+    while (in->read(image)) {
+        makeVideo << image;
+        }
+    std::cout << "Closing writer" << std::endl;
+    makeVideo.release();
+}
+
+void DoProcess(chanStage *in, chanStage *out, int frameWidth, int frameHeight) {
+    Mat image, backlogImages;
+    long n=0;
+
+    while (in->read(image)) {
+        Mat grayImage;
+        cvtColor(image, grayImage, cv::COLOR_BGR2GRAY);
+
+        if(n++==0) {
+            backlogImages=Mat::zeros(frameHeight, frameWidth, image.type());
+
+            // Display the resulting frame
+            for (int i = 0; i < frameHeight; ++i)
+            {
+                cv::Vec3b* pixel = backlogImages.ptr<cv::Vec3b>(i); // point to first pixel in row
+                uchar* pixelG = grayImage.ptr<uchar>(i);      // point to first color in row
+                for (int j = 0; j < frameWidth; ++j)       {
+                    uchar c = *pixelG++;
+                    auto &pp=pixel[j];
+                    pp[2]=c;
+                    pp[1]=c;
+                    pp[0]=c;
+                }
+            }
+
+        }
+        else   {
+            // cvtColor(frame, grayImage, cv::COLOR_BGR2GRAY);
+            // Display the resulting frame
+            for (int i = 0; i < frameHeight; ++i)    {
+                cv::Vec3b* pixel = backlogImages.ptr<cv::Vec3b>(i); // point to first pixel in row
+                uchar* pixelG = grayImage.ptr<uchar>(i);      // point to first color in row
+                for (int j = 0; j < frameWidth; ++j)       {
+                    uchar c = *pixelG++;
+                    auto &pp=pixel[j];
+                    pp[2]=pp[1];
+                    pp[0]=pp[1];
+                    pp[0]=c;
+                }
+            }
+
+        }
+    out->send( backlogImages.clone() );
+    }
+    std::cout << "Closing output" << std::endl;
+    out->close();
+}
+
 
 int main(int narg, char *sarg[]){
 
@@ -33,16 +104,18 @@ int main(int narg, char *sarg[]){
     //cap.set(CAP_PROP_FPS, 200);
 
     //
-    cv::VideoWriter makeVideo;
-    makeVideo.open(sarg[2],
-                   VideoWriter::fourcc('X','2','6','4'), 30, cv::Size(frameWidth, frameHeight), true);
+
+
+    chanStage stageRead, stageWrite;
 
 //    for(int i = 0; i < 200; i++)
     //
 
     int n=0;
     Mat backlogImages;
+    std::thread ThWriter(DoWrite, &stageWrite, sarg[2], frameWidth, frameHeight);
 
+    std::thread ThProcess(DoProcess, &stageRead, &stageWrite,  frameWidth, frameHeight);
     time_t tsBegin=clock();
 
     while(1){
@@ -50,46 +123,18 @@ int main(int narg, char *sarg[]){
         Mat frame;
         // Capture frame-by-frame
         cap >> frame;
-
-
-        // If the frame is empty, break immediately
         if (frame.empty())
             break;
-        Mat grayImage;
+        n++;
+        stageRead.send(frame);
 
-        if(n++==0) {
-            backlogImages=Mat::zeros(frame.rows, frame.cols, frame.type());
-            cvtColor(frame, grayImage, cv::COLOR_BGR2GRAY);
-            // Display the resulting frame
-            for(int x=0; x<frame.cols; x++) {
-                for (int y = 0; y < frame.rows; y++) {
-                    uchar color=grayImage.at<uchar>(y,x);
-                    Vec3b &c=backlogImages.at<Vec3b>(y,x);
-                    c[2]=color;
-                    c[1]=color;
-                    c[0]=color;
-                    }
-                }
-            }
-        else   {
-            cvtColor(frame, grayImage, cv::COLOR_BGR2GRAY);
-            // Display the resulting frame
+        // If the frame is empty, break immediately
 
-            for(int x=0; x<frame.cols; x++) {
-                for (int y = 0; y < frame.rows; y++) {
-                    uchar color=grayImage.at<uchar>(y,x);
-                    Vec3b &c=backlogImages.at<Vec3b>(y,x);
-                    c[2]=c[1];
-                    c[1]=c[0];
-                    c[0]=color;
-                }
-            }
-        }
 
 
        // imshow( "Frame", backlogImages );
       //  imshow( "FrameBW", grayImage );
-       // makeVideo << backlogImages;
+        //stage.send(backlogImages);
         if(n%100==0) {
             time_t tsNow=clock();
             long fps=n/((double)(tsNow-tsBegin)/CLOCKS_PER_SEC);
@@ -105,7 +150,9 @@ int main(int narg, char *sarg[]){
 
     // When everything done, release the video capture object
     cap.release();
-    makeVideo.release();
+    stageRead.close();
+    ThProcess.join();
+    ThWriter.join();
 
     // Closes all the frames
     //destroyAllWindows();
--- maxsrc/tchannel.hpp
+++ maxsrc/tchannel.hpp
@@ -11,67 +11,68 @@
 #include <queue>
 #include <mutex>
 //#include <cassert>
+namespace tchannel {
+    template<class T>
+    class channel_unlimited {
+        std::queue<T> inp;
+        std::mutex m;
+        std::condition_variable cv;
+        volatile bool closed = false;
+    public:
+        bool read(T &data) {
+            std::unique_lock<std::mutex> lk(m);
+            while (inp.empty()) {
+                if (closed) {
+                    cv.notify_one();
+                    lk.unlock();
+                    return false;
+                } else
+                    cv.wait(lk);
+            }
 
-template<class T> class channel_unlimited {
-    std::queue<T>           inp;
-    std::mutex              m;
-    std::condition_variable cv;
-    volatile bool  closed=false;
-public:
-    bool read(T &data) {
-        std::unique_lock<std::mutex> lk(m);
-        while(inp.empty()) {
-            if(closed) {
-                cv.notify_one();
-                lk.unlock();
+            data = inp.front();
+            inp.pop();
+            cv.notify_one();  // optional for single reader
+            lk.unlock();
+            return true;
+        }
+
+        bool send(T data) {
+            std::lock_guard<std::mutex> lk(m);
+            if (closed) {
                 return false;
             }
-            else
-                cv.wait(lk);
+            inp.push(data);
+            cv.notify_one();
+            return true;
         }
 
-        data=inp.front();
-        inp.pop();
-        cv.notify_one();  // optional for single reader
-        lk.unlock();
-        return true;
-    }
-
-    bool send(T data) {
-        std::lock_guard<std::mutex> lk(m);
-        if(closed) {
-            return false;
+        void close() {
+            std::lock_guard<std::mutex> lk(m);
+            closed = true;
+            cv.notify_one();
         }
-        inp.push(data);
-        cv.notify_one();
-        return true;
-    }
 
-    void close() {
-        std::lock_guard<std::mutex> lk(m);
-        closed=true;
-        cv.notify_one();
-    }
+    };
 
-};
+    template<class T, int limit, bool check = true>
+    class circular {
+        T buf[limit];
+        volatile int n = 0, i = 0;
+    public:
+        inline bool get(T &t) {
+            if (check) if (empty()) { return false; }
+            t = buf[i];
+            i = (i + 1) % limit;
+            n--;
+            return true;
+        }
 
-template<class T, int limit, bool check=true> class circular {
-    volatile T buf[limit];
-    volatile int n=0, i=0;
-public:
-    inline bool get(T &t) {
-        if(check) if(empty()) {  return false; }
-        t=buf[i];
-        i=(i+1)%limit;
-        n--;
-        return true;
-    }
-
-    inline bool put(T t) {
-        if(check) if(available()<=0) {  return false; }
-        buf[(i+n++)%limit]=t;
-        return true;
-    }
+        inline bool put(T t) {
+            if (check) if (available() <= 0) { return false; }
+            buf[(i + n++) % limit] = t;
+            return true;
+        }
 
 /*    bool put(T &&t) {
         if(check) if(available()<=0) return false;
@@ -80,68 +81,69 @@ public:
     }
 */
 
-    inline int available() {
-        return limit-n;
-    }
-
-    inline bool empty() {
-        return n==0;
-    }
-
-};
-
-
-template<class T, int limit> class channel_limited {
-    circular<T, limit, false>   inp;
-    std::mutex                  m;
-    std::condition_variable     waitWrite,
-            waitRead;
-    volatile bool    closed=false;
-
-public:
-    bool read(T &data) {
-        std::unique_lock<std::mutex> lk(m);
-        while(inp.empty())
-            if(closed) {
-                waitRead.notify_one();
-                lk.unlock();
-                return false;
-            } else
-                waitRead.wait(lk);
-        inp.get(data);
-        if(inp.available()==1) {
-            waitWrite.notify_one();
+        inline int available() {
+            return limit - n;
         }
-        if(!inp.empty()) {
-            waitRead.notify_one();
-        }
-        lk.unlock();
-        return true;
-    }
 
-    bool send(T data) {
-        std::unique_lock<std::mutex> lk(m);
-        while(inp.available()<=0) {
-            if (closed) {
+        inline bool empty() {
+            return n == 0;
+        }
+
+    };
+
+
+    template<class T, int limit>
+    class channel_limited {
+        tchannel::circular<T, limit, false> inp;
+        std::mutex m;
+        std::condition_variable waitWrite,
+                waitRead;
+        volatile bool closed = false;
+
+    public:
+        bool read(T &data) {
+            std::unique_lock<std::mutex> lk(m);
+            while (inp.empty())
+                if (closed) {
+                    waitRead.notify_one();
+                    lk.unlock();
+                    return false;
+                } else
+                    waitRead.wait(lk);
+            inp.get(data);
+            if (inp.available() == 1) {
                 waitWrite.notify_one();
-                lk.unlock();
-                return false;
-            } else
-                waitWrite.wait(lk);
+            }
+            if (!inp.empty()) {
+                waitRead.notify_one();
+            }
+            lk.unlock();
+            return true;
         }
-        inp.put(data);
-        waitRead.notify_one();  // optional for single writer
-        lk.unlock();
-        return true;
-    }
 
-    void close() {
-        std::lock_guard<std::mutex> lk(m);
-        closed=true;
-        waitWrite.notify_one();  // optional for single reader
-        waitRead.notify_one();  // optional for single reader
-    }
+        bool send(T data) {
+            std::unique_lock<std::mutex> lk(m);
+            while (inp.available() <= 0) {
+                if (closed) {
+                    waitWrite.notify_one();
+                    lk.unlock();
+                    return false;
+                } else
+                    waitWrite.wait(lk);
+            }
+            inp.put(data);
+            waitRead.notify_one();  // optional for single writer
+            lk.unlock();
+            return true;
+        }
 
+        void close() {
+            std::lock_guard<std::mutex> lk(m);
+            closed = true;
+            waitWrite.notify_one();  // optional for single reader
+            waitRead.notify_one();  // optional for single reader
+        }
+
+    };
 };
-
 #endif //WRITELEVEL_CHANNELS_H
